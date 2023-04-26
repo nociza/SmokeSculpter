d2b3a616cfde68d2d48c65bd6e6f61752adb75d0
pidan-bilibili <pidan@berkeley.edu>
2023-04-26 04:00:52 -0700

    into class, tried smoke particles

--- src/scene.ts
+++ src/scene.ts
@@ -1,187 +1,319 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GUI } from "dat.gui";
+import * as dat from 'dat.gui';
 
-interface SceneData {
-  scene: THREE.Scene;
-  camera: THREE.PerspectiveCamera;
-  renderer: THREE.WebGLRenderer;
-  controls: OrbitControls;
-  walls: THREE.Mesh[];
-  gui: GUI;
-  objects: THREE.Mesh[];
-}
-
-function createWalls(): THREE.Mesh[] {
-  const geometry = new THREE.BoxGeometry(10, 10, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
-  const wall1 = new THREE.Mesh(geometry, material);
-  const wall2 = wall1.clone();
-  const wall3 = wall1.clone();
-
-  wall1.position.set(0, 0, -5);
-  wall2.rotation.y = Math.PI / 2;
-  wall2.position.set(-5, 0, 0);
-  wall3.rotation.z = -Math.PI / 2;
-  wall3.rotation.x = Math.PI / 2;
-  wall3.position.set(0, -5, 0);
-
-  return [wall1, wall2, wall3];
-}
-
-function createGrid(): THREE.GridHelper {
-  const grid = new THREE.GridHelper(100, 40, 0xdddddd, 0xdddddd);
-  grid.position.set(0, -5.5, 0);
-  return grid;
-}
-
-export function initScene(): SceneData {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 10;
-  controls.maxDistance = 60;
-  controls.target.set(0, 0, 0);
-  camera.position.set(5, 5, 5);
-  controls.update();
-
-  const objects = [];
-
-  const walls = createWalls();
-  walls.forEach((wall) => scene.add(wall));
-
-  //add a mesh grid to the bottom
-  const grid_floor = createGrid();
-  scene.add(grid_floor)
-
-  // Add spot lights to the scene
-  const spotLight = new THREE.SpotLight(0xffffff, 0.5);
-  spotLight.position.set(0, 10, 0);
-  scene.add(spotLight);
-
-  // Add ambient light to the scene
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
+export class SmokeSimulation {
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
+  private controls: OrbitControls;
+  private particles: THREE.Points;
+  private gui: dat.GUI;
+  private guiData: { color: string; size: number };
+  private spotLight: THREE.SpotLight;
+  private grid: THREE.GridHelper;
+  private walls: THREE.Mesh[] = [];
+  private objects: THREE.Mesh[] = [];
+  private mouseposition = { x: 0, y: 0 };
 
 
-  const gui = setupGUI(walls, spotLight, grid_floor);
+  constructor() {
+    this.initScene();
+    document.body.appendChild(this.renderer.domElement);
+    this.spotLight = new THREE.SpotLight(0xffffff, 0.5);
+    this.initGUI();
 
-  return { scene, camera, renderer, controls, walls, gui, objects };
-}
+    this.createParticles();
+    this.animate();
 
-export function renderScene({
-  scene,
-  camera,
-  renderer,
-  controls,
-}: SceneData): void {
-  const animate = () => {
-    requestAnimationFrame(animate);
+    //window.addEventListener('resize', this.onWindowResize.bind(this), false);
+    //window.addEventListener('click', this.onMouseClick.bind(this), false);
 
-    controls.update();
+    window.addEventListener("click", (event) => {
+      this.onMouseClick(event);
+    });
 
-    renderer.render(scene, camera);
+    window.addEventListener("keypress", (event) => {
+      this.handleKeyPress(this.mouseposition, event);
+    });
+
+    window.addEventListener("mousemove", (event) => {
+      this.mouseposition.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouseposition.y = -((event.clientY / window.innerHeight) * 2 - 1);
+    })
+
+  }
+
+  private addNewObject(mouseposition: any, event: KeyboardEvent): void {
+    //https://stackoverflow.com/questions/47682260/three-js-draw-where-mouse-clicks-but-entirely-parallel-to-camera-orientation
+    //something might be off
+    console.log(mouseposition);
+    var raycaster = new THREE.Raycaster();
+    var plane = new THREE.Plane();
+    var planeNormal = new THREE.Vector3();
+    var point = new THREE.Vector3();
+    var mouse = new THREE.Vector2(mouseposition.x, mouseposition.y);
+    planeNormal.copy(this.camera.position).normalize();
+    plane.setFromNormalAndCoplanarPoint(planeNormal, this.scene.position);
+    raycaster.setFromCamera(mouse, this.camera);
+    raycaster.ray.intersectPlane(plane, point);
+
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
+    const cube = new THREE.Mesh(geometry, material);
+
+    cube.position.copy(point);
+    this.objects.push(cube);
+    this.scene.add(cube);
+  }
+
+
+  private createParticles(): void {
+    const geometry = new THREE.BufferGeometry();
+    const count = 200;
+    const positions = new Float32Array(count * 3);
+    const sizes = new Float32Array(count);
+
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 10;
+      sizes[i] = 0.1;
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('size', new THREE.BufferAttribute(sizes, 1));
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        color: { value: new THREE.Color(0xffffff) },
+        texture: { value: null }
+      },
+      vertexShader: `
+                attribute float size;
+                varying vec2 vUv;
+                void main() {
+                    vUv = uv;
+                    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+                    gl_PointSize = size * (300.0 / -mvPosition.z);
+                    gl_Position = projectionMatrix * mvPosition;
+                }
+            `,
+      fragmentShader: `
+                uniform vec3 color;
+                uniform sampler2D texture;
+                varying vec2 vUv;
+                void main() {
+                    gl_FragColor = vec4(color, 1.0) * texture2D(texture, gl_PointCoord);
+                }
+            `,
+      blending: THREE.AdditiveBlending,
+      depthTest: false,
+      transparent: true
+    });
+
+    this.particles = new THREE.Points(geometry, material);
+    this.scene.add(this.particles);
+  }
+
+
+
+  private onMouseClick(event: MouseEvent): void {
+    const vector = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1,
+      -(event.clientY / window.innerHeight) * 2 + 1, 0.5);
+
+    vector.unproject(this.camera);
+    const dir = vector.sub(this.camera.position).normalize();
+    const distance = -this.camera.position.z / dir.z;
+    const pos = this.camera.position.clone().add(dir.multiplyScalar(distance));
+
+    this.particles.position.copy(pos);
+  }
+
+
+
+
+  private createGrid(): void {
+    this.grid = new THREE.GridHelper(100, 40, 0xdddddd, 0xdddddd);
+    this.grid.position.set(0, -5.5, 0);
+    this.scene.add(this.grid);
+  }
+
+  private createWalls(): void {
+    const geometry = new THREE.BoxGeometry(10, 10, 1);
+    const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
+    const wall1 = new THREE.Mesh(geometry, material);
+    const wall2 = wall1.clone();
+    const wall3 = wall1.clone();
+
+    wall1.position.set(0, 0, -5);
+    wall2.rotation.y = Math.PI / 2;
+    wall2.position.set(-5, 0, 0);
+    wall3.rotation.z = -Math.PI / 2;
+    wall3.rotation.x = Math.PI / 2;
+    wall3.position.set(0, -5, 0);
+
+    this.walls = [wall1, wall2, wall3];
+  }
+
+
+
+  private initGUI(): void {
+    this.gui = new dat.GUI();
+    this.guiData = {
+      color: '#ffffff',
+      size: 0.1
+    };
+
+    const wallColors = "rgb(128, 128, 128)";
+    this.gui
+      .addColor(new THREE.Color(wallColors), "r")
+      .onChange((value) => {
+        // parse color to hex and set it to the walls
+        const { r, g, b } = this.parseColor(value);
+        this.walls.forEach((wall) => {
+          (wall.material as THREE.MeshBasicMaterial).color.setRGB(r, g, b);
+        });
+      })
+      .name("Wall Color");
+
+    // Spot light color picker
+    const spotLightColor = "rgb(255, 255, 255)";
+    this.spotLight.position.set(0, 10, 0);
+    this.scene.add(this.spotLight);
+    this.gui
+      .addColor(new THREE.Color(spotLightColor), "r")
+      .onChange((value) => {
+        const { r, g, b } = this.parseColor(value);
+        this.spotLight.color.setRGB(r, g, b);
+      })
+      .name("Spot Light Color");
+
+    // change the direction of the light using GUI
+    this.gui.add(this.spotLight.position, "x", -10, 10);
+    this.gui.add(this.spotLight.position, "y", -10, 10);
+    this.gui.add(this.spotLight.position, "z", -10, 10);
+
+    //toggle grid visibility
+    this.gui.add(this.grid, "visible");
+
+    this.gui
+      .addColor(this.guiData, 'color')
+      .name('Smoke Color')
+      .onChange((color: string) => {
+        (this.particles.material as THREE.ShaderMaterial).uniforms.color.value.set(color);
+      });
+
+    this.gui
+      .add(this.guiData, 'size', 0.01, 1.0, 0.01)
+      .name('Smoke Size')
+      .onChange((size: number) => {
+        const sizes = (this.particles.geometry as THREE.BufferGeometry).attributes.size.itemSize;
+        for (let i = 0; i < sizes; i++) {
+          sizes[i] = size;
+        }
+        this.particles.geometry.attributes.size.needsUpdate = true;
+      });
+  }
+
+
+
+  private initScene(): void {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.minDistance = 10;
+    this.controls.maxDistance = 60;
+    this.controls.target.set(0, 0, 0);
+    this.camera.position.set(5, 5, 5);
+    this.controls.update();
+
+    const objects = [];
+
+    this.createWalls();
+    this.walls.forEach((wall) => this.scene.add(wall));
+
+    // add a mesh grid to the bottom
+    this.createGrid();
+
+    // Add spot lights to the scene
+    const spotLight = new THREE.SpotLight(0xffffff, 0.5);
+    spotLight.position.set(0, 10, 0);
+    this.scene.add(spotLight);
+
+    // Add ambient light to the scene
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    this.scene.add(ambientLight);
+  }
+
+
+
+
+  private animate(): void {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.updateParticles();
+    this.controls.update();
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+
+
+
+  private updateParticles(): void {
+    const positions = this.particles.geometry.attributes.position;
+    const count = positions.itemSize / 3;
+
+    for (let i = 0; i < count; i++) {
+      positions[i * 3 + 1] += 0.1;
+      if (positions[i * 3 + 1] > 5) {
+        positions[i * 3 + 1] = -5;
+      }
+    }
+
+    this.particles.geometry.attributes.position.needsUpdate = true;
+  }
+
+  private resetCamera() {
+    this.camera.position.set(5, 5, 5);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+  }
+
+
+  private handleKeyPress(
+    mouseposition: any,
+    event: KeyboardEvent
+  ): void {
+    console.log(event);
+    if (event?.code === "Space") {
+      this.resetCamera();
+    }
+    if (event?.code === "KeyN") {
+      this.addNewObject(mouseposition, event);
+    }
+  }
+
+
+
+
+  private onWindowResize(): void {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  /******************************************************************************
+   *                            Helper functions                                *
+   ******************************************************************************/
+  private parseColor = (value: number) => {
+    const r = parseInt(value.toString(16).substring(0, 2), 16) / 255;
+    const g = parseInt(value.toString(16).substring(2, 4), 16) / 255;
+    const b = parseInt(value.toString(16).substring(4, 6), 16) / 255;
+    return { r, g, b };
   };
 
-  animate();
+
 }
-
-function addNewObject(sceneDat: SceneData, mouseposition: any, event: KeyboardEvent): void {
-  //https://stackoverflow.com/questions/47682260/three-js-draw-where-mouse-clicks-but-entirely-parallel-to-camera-orientation
-  //something might be off
-  console.log(mouseposition);
-  const scene = sceneDat.scene;
-  const camera = sceneDat.camera;
-  var raycaster = new THREE.Raycaster();
-  var plane = new THREE.Plane();
-  var planeNormal = new THREE.Vector3();
-  var point = new THREE.Vector3();
-  var mouse = new THREE.Vector2(mouseposition.x, mouseposition.y);
-  planeNormal.copy(camera.position).normalize();
-  plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
-  raycaster.setFromCamera(mouse, camera);
-  raycaster.ray.intersectPlane(plane, point);
-
-  const geometry = new THREE.BoxGeometry(2, 2, 2);
-  const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
-  const cube = new THREE.Mesh(geometry, material);
-
-  cube.position.copy(point);
-  sceneDat.objects.push(cube);
-  scene.add(cube);
-}
-
-function setupGUI(walls: THREE.Mesh[], spotLight: THREE.SpotLight, grid: THREE.GridHelper): GUI {
-  const gui = new GUI();
-
-  // Wall color picker (the label should say "Wall color")
-  const wallColors = "rgb(128, 128, 128)";
-  gui
-    .addColor(new THREE.Color(wallColors), "r")
-    .onChange((value) => {
-      // parse color to hex and set it to the walls
-      const { r, g, b } = parseColor(value);
-      walls.forEach((wall) => {
-        (wall.material as THREE.MeshBasicMaterial).color.setRGB(r, g, b);
-      });
-    })
-    .name("Wall Color");
-
-  // Spot light color picker
-  const spotLightColor = "rgb(255, 255, 255)";
-  gui
-    .addColor(new THREE.Color(spotLightColor), "r")
-    .onChange((value) => {
-      const { r, g, b } = parseColor(value);
-      spotLight.color.setRGB(r, g, b);
-    })
-    .name("Spot Light Color");
-  // change the direction of the light using GUI
-  gui.add(spotLight.position, "x", -10, 10);
-  gui.add(spotLight.position, "y", -10, 10);
-  gui.add(spotLight.position, "z", -10, 10);
-
-  //toggle grid visibility
-  gui.add(grid, "visible");
-
-  return gui;
-}
-
-function resetCamera(camera: THREE.PerspectiveCamera) {
-  camera.position.set(5, 5, 5);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-}
-
-export function handleKeyPress(
-  sceneData: SceneData,
-  mouseposition: any,
-  event: KeyboardEvent
-): void {
-  let camera = sceneData.camera;
-  console.log(event);
-  if (event?.code === "Space") {
-    resetCamera(camera);
-  }
-  if (event?.code === "KeyN") {
-    addNewObject(sceneData, mouseposition, event);
-  }
-}
-
-/******************************************************************************
- *                            Helper functions                                *
- ******************************************************************************/
-const parseColor = (value: number) => {
-  const r = parseInt(value.toString(16).substring(0, 2), 16) / 255;
-  const g = parseInt(value.toString(16).substring(2, 4), 16) / 255;
-  const b = parseInt(value.toString(16).substring(4, 6), 16) / 255;
-  return { r, g, b };
-};
