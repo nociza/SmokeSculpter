89ecce065a8fb72a42bc87de5ded2d091d48d8b7
nociza <azicon@berkeley.edu>
2023-04-30 02:18:02 -0700

    Basic structure in place

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,34 @@
-import { initScene, renderScene, handleKeyPress } from "./scene";
-var mouseposition = {x:0, y:0}
-const sceneData = initScene();
-renderScene(sceneData);
+import { initScene, handleKeyPress, updateScene } from "./scene";
+import * as THREE from "three";
 
-// Add an event listener for keypress events
-window.addEventListener("keypress", (event) => {
-  handleKeyPress(sceneData, mouseposition, event);
-});
+var mouseposition = { x: 0, y: 0 };
 
-// Track mouse position
-window.addEventListener("mousemove", (event) => {
-  mouseposition.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouseposition.y = -((event.clientY / window.innerHeight) * 2 - 1);
-})
+(async () => {
+  const fluidSimData = await initScene();
+  const sceneData = fluidSimData.sceneData;
+
+  // Add an event listener for keypress events
+  window.addEventListener("keypress", (event) => {
+    handleKeyPress(sceneData, mouseposition, event);
+  });
+
+  // Track mouse position
+  window.addEventListener("mousemove", (event) => {
+    mouseposition.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouseposition.y = -((event.clientY / window.innerHeight) * 2 - 1);
+  });
+
+  const clock = new THREE.Clock();
+  const animate = () => {
+    requestAnimationFrame(animate);
+    const delta = clock.getDelta();
+    const elapsedTime = clock.getElapsedTime();
+
+    updateScene(elapsedTime, delta, fluidSimData);
+
+    sceneData.renderer.render(sceneData.scene, sceneData.camera);
+    sceneData.controls.update();
+  };
+
+  animate();
+})();
--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GUI } from "dat.gui";
+import { loadShaders, Shaders, createFullscreenQuad } from "./shaders";
 
 interface SceneData {
   scene: THREE.Scene;
@@ -10,32 +11,37 @@ interface SceneData {
   walls: THREE.Mesh[];
   gui: GUI;
   objects: THREE.Mesh[];
+  // Fluid simulation
+  velocityFramebuffer1: THREE.WebGLRenderTarget;
+  velocityFramebuffer2: THREE.WebGLRenderTarget;
+  pressureFramebuffer1: THREE.WebGLRenderTarget;
+  pressureFramebuffer2: THREE.WebGLRenderTarget;
+  divergenceFramebuffer: THREE.WebGLRenderTarget;
+  velocityTexture1: THREE.Texture;
+  velocityTexture2: THREE.Texture;
+  pressureTexture1: THREE.Texture;
+  pressureTexture2: THREE.Texture;
+  divergenceTexture: THREE.Texture;
+  fullscreenQuad: THREE.Mesh;
+  iterations: number;
 }
 
-function createWalls(): THREE.Mesh[] {
-  const geometry = new THREE.BoxGeometry(10, 10, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
-  const wall1 = new THREE.Mesh(geometry, material);
-  const wall2 = wall1.clone();
-  const wall3 = wall1.clone();
-
-  wall1.position.set(0, 0, -5);
-  wall2.rotation.y = Math.PI / 2;
-  wall2.position.set(-5, 0, 0);
-  wall3.rotation.z = -Math.PI / 2;
-  wall3.rotation.x = Math.PI / 2;
-  wall3.position.set(0, -5, 0);
-
-  return [wall1, wall2, wall3];
+interface FluidSimData {
+  sceneData: SceneData;
+  shaders: Shaders;
 }
 
-function createGrid(): THREE.GridHelper {
-  const grid = new THREE.GridHelper(100, 40, 0xdddddd, 0xdddddd);
-  grid.position.set(0, -5.5, 0);
-  return grid;
+export interface EulerianGrid {
+  size: THREE.Vector3;
+  data: Float32Array;
 }
 
-export function initScene(): SceneData {
+export function createEulerianGrid(size: THREE.Vector3): EulerianGrid {
+  const data = new Float32Array(size.x * size.y * size.z * 4);
+  return { size, data };
+}
+
+export async function initScene(): Promise<FluidSimData> {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -59,43 +65,200 @@ export function initScene(): SceneData {
   const walls = createWalls();
   walls.forEach((wall) => scene.add(wall));
 
-  //add a mesh grid to the bottom
-  const grid_floor = createGrid();
-  scene.add(grid_floor)
+  // create 3D eulerian grid
+  const size = new THREE.Vector3(64, 64, 64);
+  const grid = createEulerianGrid(size);
 
-  // Add spot lights to the scene
-  const spotLight = new THREE.SpotLight(0xffffff, 0.5);
-  spotLight.position.set(0, 10, 0);
-  scene.add(spotLight);
+  const gui = new GUI();
 
-  // Add ambient light to the scene
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
+  // tslint: disable - line;
+  gui.add(grid, "size", 0, 128, 1).onChange(() => {
+    console.log(grid.size);
+  });
 
+  const shaders = await loadShaders();
 
-  const gui = setupGUI(walls, spotLight, grid_floor);
-
-  return { scene, camera, renderer, controls, walls, gui, objects };
-}
-
-export function renderScene({
-  scene,
-  camera,
-  renderer,
-  controls,
-}: SceneData): void {
-  const animate = () => {
-    requestAnimationFrame(animate);
-
-    controls.update();
-
-    renderer.render(scene, camera);
+  // Create framebuffers and textures
+  const renderTargetOptions = {
+    format: THREE.RGBAFormat,
+    type: THREE.FloatType,
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    stencilBuffer: false,
   };
 
-  animate();
+  const velocityFramebuffer1 = new THREE.WebGLRenderTarget(
+    window.innerWidth,
+    window.innerHeight,
+    renderTargetOptions
+  );
+  const velocityFramebuffer2 = new THREE.WebGLRenderTarget(
+    window.innerWidth,
+    window.innerHeight,
+    renderTargetOptions
+  );
+  const pressureFramebuffer1 = new THREE.WebGLRenderTarget(
+    window.innerWidth,
+    window.innerHeight,
+    renderTargetOptions
+  );
+  const pressureFramebuffer2 = new THREE.WebGLRenderTarget(
+    window.innerWidth,
+    window.innerHeight,
+    renderTargetOptions
+  );
+  const divergenceFramebuffer = new THREE.WebGLRenderTarget(
+    window.innerWidth,
+    window.innerHeight,
+    renderTargetOptions
+  );
+
+  const velocityTexture1 = velocityFramebuffer1.texture;
+  const velocityTexture2 = velocityFramebuffer2.texture;
+  const pressureTexture1 = pressureFramebuffer1.texture;
+  const pressureTexture2 = pressureFramebuffer2.texture;
+  const divergenceTexture = divergenceFramebuffer.texture;
+
+  const fullscreenQuad = createFullscreenQuad();
+
+  const sceneData: SceneData = {
+    scene,
+    camera,
+    renderer,
+    controls,
+    walls,
+    gui,
+    objects,
+    // Add new properties
+    velocityFramebuffer1,
+    velocityFramebuffer2,
+    pressureFramebuffer1,
+    pressureFramebuffer2,
+    divergenceFramebuffer,
+    velocityTexture1,
+    velocityTexture2,
+    pressureTexture1,
+    pressureTexture2,
+    divergenceTexture,
+    fullscreenQuad,
+    iterations: 20,
+  };
+
+  return { sceneData, shaders };
 }
 
-function addNewObject(sceneDat: SceneData, mouseposition: any, event: KeyboardEvent): void {
+export function updateScene(
+  time: number,
+  delta: number,
+  fluidSimData: FluidSimData
+): void {
+  const sceneData = fluidSimData.sceneData;
+  const shaders = fluidSimData.shaders;
+
+  // 1. Advect velocity
+  shaders.advection.uniforms.u_velocity.value = sceneData.velocityTexture1;
+  sceneData.fullscreenQuad.material = shaders.advection;
+  sceneData.scene.add(sceneData.fullscreenQuad);
+  sceneData.renderer.setRenderTarget(sceneData.velocityFramebuffer2);
+  sceneData.renderer.render(sceneData.scene, sceneData.camera);
+  sceneData.scene.remove(sceneData.fullscreenQuad);
+  swapTextures(sceneData, "velocity");
+
+  // 2. Splat
+  shaders.splat.uniforms.u_target.value = sceneData.velocityTexture1;
+  sceneData.fullscreenQuad.material = shaders.splat;
+  sceneData.scene.add(sceneData.fullscreenQuad);
+  sceneData.renderer.setRenderTarget(sceneData.velocityFramebuffer2);
+  sceneData.renderer.render(sceneData.scene, sceneData.camera);
+  sceneData.scene.remove(sceneData.fullscreenQuad);
+  swapTextures(sceneData, "velocity");
+
+  // 3. Calculate divergence
+  shaders.divergence.uniforms.u_velocity.value = sceneData.velocityTexture1;
+  sceneData.fullscreenQuad.material = shaders.divergence;
+  sceneData.scene.add(sceneData.fullscreenQuad);
+  sceneData.renderer.setRenderTarget(sceneData.divergenceFramebuffer);
+  sceneData.renderer.render(sceneData.scene, sceneData.camera);
+  sceneData.scene.remove(sceneData.fullscreenQuad);
+
+  // 4. Solve pressure
+  for (let i = 0; i < sceneData.iterations; i++) {
+    shaders.pressure.uniforms.u_pressure.value = sceneData.pressureTexture1;
+    shaders.pressure.uniforms.u_divergence.value = sceneData.divergenceTexture;
+    sceneData.fullscreenQuad.material = shaders.pressure;
+    sceneData.scene.add(sceneData.fullscreenQuad);
+    sceneData.renderer.setRenderTarget(sceneData.pressureFramebuffer2);
+    sceneData.renderer.render(sceneData.scene, sceneData.camera);
+    sceneData.scene.remove(sceneData.fullscreenQuad);
+    swapTextures(sceneData, "pressure");
+  }
+
+  // 5. Subtract gradient
+  shaders.gradientSubtract.uniforms.u_pressure.value =
+    sceneData.pressureTexture1;
+  shaders.gradientSubtract.uniforms.u_velocity.value =
+    sceneData.velocityTexture1;
+  sceneData.fullscreenQuad.material = shaders.gradientSubtract;
+  sceneData.scene.add(sceneData.fullscreenQuad);
+  sceneData.renderer.setRenderTarget(sceneData.velocityFramebuffer2);
+  sceneData.renderer.render(sceneData.scene, sceneData.camera);
+  sceneData.scene.remove(sceneData.fullscreenQuad);
+  swapTextures(sceneData, "velocity");
+}
+
+function swapTextures(sceneData: SceneData, type: string): void {
+  if (type === "velocity") {
+    [sceneData.velocityTexture1, sceneData.velocityTexture2] = [
+      sceneData.velocityTexture2,
+      sceneData.velocityTexture1,
+    ];
+    [sceneData.velocityFramebuffer1, sceneData.velocityFramebuffer2] = [
+      sceneData.velocityFramebuffer2,
+      sceneData.velocityFramebuffer1,
+    ];
+  } else if (type === "pressure") {
+    [sceneData.pressureTexture1, sceneData.pressureTexture2] = [
+      sceneData.pressureTexture2,
+      sceneData.pressureTexture1,
+    ];
+    [sceneData.pressureFramebuffer1, sceneData.pressureFramebuffer2] = [
+      sceneData.pressureFramebuffer2,
+      sceneData.pressureFramebuffer1,
+    ];
+  }
+}
+/******************************************************************************
+ *                            Helper functions                                *
+ ******************************************************************************/
+const parseColor = (value: number) => {
+  const r = parseInt(value.toString(16).substring(0, 2), 16) / 255;
+  const g = parseInt(value.toString(16).substring(2, 4), 16) / 255;
+  const b = parseInt(value.toString(16).substring(4, 6), 16) / 255;
+  return { r, g, b };
+};
+
+function createShaderMaterial(
+  vertexShader: string,
+  fragmentShader: string,
+  uniforms: any
+): THREE.ShaderMaterial {
+  return new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms,
+  });
+}
+
+async function loadShaderFile(shaderPath: string): Promise<string> {
+  const response = await fetch(shaderPath);
+  return response.text();
+}
+
+function addNewObject(
+  sceneDat: SceneData,
+  mouseposition: any,
+  event: KeyboardEvent
+): void {
   //https://stackoverflow.com/questions/47682260/three-js-draw-where-mouse-clicks-but-entirely-parallel-to-camera-orientation
   //something might be off
   console.log(mouseposition);
@@ -120,47 +283,6 @@ function addNewObject(sceneDat: SceneData, mouseposition: any, event: KeyboardEv
   scene.add(cube);
 }
 
-function setupGUI(walls: THREE.Mesh[], spotLight: THREE.SpotLight, grid: THREE.GridHelper): GUI {
-  const gui = new GUI();
-
-  // Wall color picker (the label should say "Wall color")
-  const wallColors = "rgb(128, 128, 128)";
-  gui
-    .addColor(new THREE.Color(wallColors), "r")
-    .onChange((value) => {
-      // parse color to hex and set it to the walls
-      const { r, g, b } = parseColor(value);
-      walls.forEach((wall) => {
-        (wall.material as THREE.MeshBasicMaterial).color.setRGB(r, g, b);
-      });
-    })
-    .name("Wall Color");
-
-  // Spot light color picker
-  const spotLightColor = "rgb(255, 255, 255)";
-  gui
-    .addColor(new THREE.Color(spotLightColor), "r")
-    .onChange((value) => {
-      const { r, g, b } = parseColor(value);
-      spotLight.color.setRGB(r, g, b);
-    })
-    .name("Spot Light Color");
-  // change the direction of the light using GUI
-  gui.add(spotLight.position, "x", -10, 10);
-  gui.add(spotLight.position, "y", -10, 10);
-  gui.add(spotLight.position, "z", -10, 10);
-
-  //toggle grid visibility
-  gui.add(grid, "visible");
-
-  return gui;
-}
-
-function resetCamera(camera: THREE.PerspectiveCamera) {
-  camera.position.set(5, 5, 5);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-}
-
 export function handleKeyPress(
   sceneData: SceneData,
   mouseposition: any,
@@ -176,12 +298,30 @@ export function handleKeyPress(
   }
 }
 
-/******************************************************************************
- *                            Helper functions                                *
- ******************************************************************************/
-const parseColor = (value: number) => {
-  const r = parseInt(value.toString(16).substring(0, 2), 16) / 255;
-  const g = parseInt(value.toString(16).substring(2, 4), 16) / 255;
-  const b = parseInt(value.toString(16).substring(4, 6), 16) / 255;
-  return { r, g, b };
-};
+function resetCamera(camera: THREE.PerspectiveCamera) {
+  camera.position.set(5, 5, 5);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+function createWalls(): THREE.Mesh[] {
+  const geometry = new THREE.BoxGeometry(10, 10, 1);
+  const material = new THREE.MeshPhongMaterial({ color: 0x808080 });
+  const wall1 = new THREE.Mesh(geometry, material);
+  const wall2 = wall1.clone();
+  const wall3 = wall1.clone();
+
+  wall1.position.set(0, 0, -5);
+  wall2.rotation.y = Math.PI / 2;
+  wall2.position.set(-5, 0, 0);
+  wall3.rotation.z = -Math.PI / 2;
+  wall3.rotation.x = Math.PI / 2;
+  wall3.position.set(0, -5, 0);
+
+  return [wall1, wall2, wall3];
+}
+
+function createGrid(): THREE.GridHelper {
+  const grid = new THREE.GridHelper(100, 40, 0xdddddd, 0xdddddd);
+  grid.position.set(0, -5.5, 0);
+  return grid;
+}
